ce8c301f4d64fe8971782d10ae0cbf8b53d4b8d9
Cliffano Subagio <cliffano@gmail.com>
2016-06-28 10:41:53 +1000

    Add flush and replication agent crud tests.

---
+++ test/ruby/flush_agent_spec.rb
@@ -0,0 +1,79 @@
+require_relative 'spec_helper'
+
+describe 'Flush Agent' do
+  before do
+    init_client
+    @sling = SwaggerAemClient::SlingApi.new
+
+    # ensure agent doesn't exist prior to testing
+    begin
+      data, status_code, headers = @sling.delete_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-flush-agent'
+      )
+      # delete agent when it exists
+      expect([200, 204]).to include(status_code)
+    rescue SwaggerAemClient::ApiError => err
+      # ignore when agent does not exist
+      expect(err.code).to eq(404)
+    end
+
+    begin
+      data, status_code, headers = @sling.get_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-flush-agent'
+      )
+    rescue SwaggerAemClient::ApiError => err
+      expect(err.code).to eq(404)
+    end
+
+  end
+
+  after do
+  end
+
+  describe 'test create' do
+
+    it 'should succeed an update' do
+
+      # create a new flush agent
+      data, status_code, headers = @sling.post_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-flush-agent',
+        {
+          :jcrprimary_type => 'cq:Page',
+          :jcrcontentcqname => 'some-flush-agent',
+          :jcrcontentjcrtitle => 'Some Flush Agent Title',
+          :jcrcontentjcrdescription => 'Some Flush Agent Description',
+          :jcrcontentslingresource_type => '/libs/cq/replication/components/agent',
+          :jcrcontenttransport_uri => 'http://somehost:8080/dispatcher/invalidate.cache',
+          :jcrcontentlog_level => 'error',
+          :jcrcontentno_versioning => true,
+          :jcrcontentprotocol_http_headers_type_hint => 'String[]',
+          :jcrcontentprotocol_http_method => 'GET',
+          :jcrcontentretry_delay => '30000',
+          :jcrcontentserialization_type => 'flush',
+          :jcrcontentjcrmixin_types => 'cq:ReplicationStatus',
+          :jcrcontenttrigger_receive => true,
+          :jcrcontenttrigger_specific => true,
+          :jcrcontentcqtemplate => '/libs/cq/replication/templates/agent',
+          :jcrcontentenabled => true
+        }
+      )
+      expect(status_code).to eq(201)
+
+      # update an existing flush agent
+      data, status_code, headers = @sling.post_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-flush-agent',
+        {
+          :jcrprimary_type => 'cq:Page',
+          :jcrcontentcqname => 'some-flush-agent',
+          :jcrcontentjcrtitle => 'Some New Flush Agent Title',
+          :jcrcontentjcrdescription => 'Some New Flush Agent Description'        }
+      )
+      expect(status_code).to eq(200)
+    end
+
+  end
+end
---
+++ test/ruby/replication_agent_spec.rb
@@ -0,0 +1,69 @@
+require_relative 'spec_helper'
+
+describe 'Replication Agent' do
+  before do
+    init_client
+    @sling = SwaggerAemClient::SlingApi.new
+
+    # ensure agent doesn't exist prior to testing
+    begin
+      data, status_code, headers = @sling.delete_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-replication-agent'
+      )
+      # delete agent when it exists
+      expect(status_code).to eq(200)
+    rescue SwaggerAemClient::ApiError => err
+      # ignore when agent does not exist
+      expect(err.code).to eq(404)
+    end
+
+    begin
+      data, status_code, headers = @sling.get_agent_with_http_info(
+        runmode = 'author',
+        name = 'some-replication-agent'
+      )
+    rescue SwaggerAemClient::ApiError => err
+      expect(err.code).to eq(404)
+    end
+
+  end
+
+  after do
+  end
+
+  it 'should succeed an update' do
+
+    # create a new replication agent
+    data, status_code, headers = @sling.post_agent_with_http_info(
+      runmode = 'author',
+      name = 'some-replication-agent',
+      {
+        :jcrprimary_type => 'cq:Page',
+        :jcrcontentcqname => 'some-replication-agent',
+        :jcrcontentjcrtitle => 'Some Replication Agent Title',
+        :jcrcontentjcrdescription => 'Some Replication Agent Description',
+        :jcrcontentslingresource_type => '/libs/cq/replication/components/agent',
+        :jcrcontentserialization_type => 'durbo',
+        :jcrcontenttransport_uri => 'http://somehost:8080/bin/receive?sling:authRequestLogin=1',
+        :jcrcontentlog_level => 'error',
+        :jcrcontentretry_delay => '30000',
+        :jcrcontentcqtemplate => '/libs/cq/replication/templates/agent',
+        :jcrcontentenabled => true
+      }
+    )
+    expect(status_code).to eq(201)
+
+    # update an existing replication agent
+    data, status_code, headers = @sling.post_agent_with_http_info(
+      runmode = 'author',
+      name = 'some-replication-agent',
+      {
+        :jcrprimary_type => 'cq:Page',
+        :jcrcontentcqname => 'some-replication-agent',
+        :jcrcontentjcrtitle => 'Some New Replication Agent Title',
+        :jcrcontentjcrdescription => 'Some New Replication Agent Description'        }
+    )
+    expect(status_code).to eq(200)
+  end
+end
