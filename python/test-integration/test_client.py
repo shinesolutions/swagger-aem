759213056260a4fbdf7a6754a6dedf017cb22f2c
Cliffano Subagio <cliffano@gmail.com>
2016-09-01 23:39:24 +1000

    Add client test failing auth scenario.

---
+++ python/test-integration/test_client.py
@@ -0,0 +1,18 @@
+import helper
+import swaggeraem
+from swaggeraem.rest import ApiException
+import unittest
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        swaggeraem.configuration.username = 'someinexistingusername'
+        swaggeraem.configuration.password = 'someinexistingpassword'
+        client = swaggeraem.ApiClient('http://localhost:4502')
+        self.sling = swaggeraem.apis.SlingApi(helper.init_client())
+
+    def test_api_call_should_error_when_credential_is_incorrect(self):
+        try:
+            self.sling.post_tree_activation_with_http_info(True, False, '/etc/designs/geometrixx')
+        except ApiException as err:
+            self.assertEqual(err.status, 401)
