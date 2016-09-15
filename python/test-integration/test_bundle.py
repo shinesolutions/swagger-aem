e2e5803f40af1a4d049574442a244151d7797f03
Cliffano Subagio <cliffano@gmail.com>
2016-09-15 23:17:29 +1000

    [python] Update api init to declare client.

--- python/test-integration/test_bundle.py
+++ python/test-integration/test_bundle.py
@@ -6,7 +6,8 @@ import unittest
 class TestBundleStop(unittest.TestCase):
 
     def setUp(self):
-        self.console = swaggeraem.apis.ConsoleApi(helper.init_client())
+        client = helper.init_client()
+        self.console = swaggeraem.apis.ConsoleApi(client)
 
     def test_should_succeed_when_bundle_exists(self):
         response = self.console.post_bundle_with_http_info('com.adobe.cq.social.cq-social-forum', 'stop')
