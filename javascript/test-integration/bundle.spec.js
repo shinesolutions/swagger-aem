beb26c28cb6811c94b643f8eb63aaede19593a14
Cliffano Subagio <cliffano@gmail.com>
2016-11-21 23:55:53 +1100

    [javascript] Add integration test structure and make target executing mochajs.

---
+++ javascript/test-integration/bundle.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var NodeSwaggerAem = require('../generated/src/index');
+
+var console;
+
+beforeEach(function() {
+  console = new NodeSwaggerAem.ConsoleApi();
+});
+
+describe('Bundle', function() {
+  describe('test bundle stop', function() {
+    it('should succeed when bundle exists', function(done) {
+      done();
+    });
+  });
+});
