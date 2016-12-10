b85d948bd0283bca285e81b69024c9265c9e06b8
Cliffano Subagio <cliffano@gmail.com>
2016-12-11 00:04:25 +1100

    [java] Remove commented out code.

--- java/test-integration/src/test/java/com/shinesolutions/swaggeraem4j/TestHelper.java
+++ java/test-integration/src/test/java/com/shinesolutions/swaggeraem4j/TestHelper.java
@@ -87,16 +87,4 @@ public class TestHelper {
 			public String name;
 		}
 	}
-
-	/*
-	 * def find_authorizable_id(sling, path, name) begin data, status_code,
-	 * headers = sling.post_query_with_http_info( path = path, p_limit = -1,
-	 * _1_property = 'rep:authorizableId', _1_property_value = name )
-	 * expect(status_code).to eq(200)
-	 * 
-	 * data = JSON.parse(data) if data['success'] == true && data['hits'].length
-	 * == 1 authorizable_id = data['hits'][0]['name'] return authorizable_id end
-	 * rescue SwaggerAemClient::ApiError => err # ignore when authorizable does
-	 * not exist expect(err.code).to eq(404) end end
-	 */
 }
