06ba6566b9a7dd323b7e913583a81d9424ca5f41
Cliffano Subagio <cliffano@gmail.com>
2017-03-13 10:24:01 +1100

    [java] Fix missing getLoginPage operation response body.

--- java/test-integration/src/test/java/com/shinesolutions/swaggeraem4j/AemTest.java
+++ java/test-integration/src/test/java/com/shinesolutions/swaggeraem4j/AemTest.java
@@ -19,10 +19,7 @@ public class AemTest {
 	public void testGetLoginPage() throws ApiException {
 		ApiResponse<Void> response = cq.getLoginPageWithHttpInfo();
 		assertEquals(200, response.getStatusCode());
-    // TODO: investigate if generated Java client also doesn't return data due to swallowed return type
-    //       https://github.com/shinesolutions/swagger-aem/commit/6b1dbf00c27930627ac0702f65e0a560761c1bf0#diff-db60b8b509c67ab54bfcc8564c866537R62
-    // System.out.println(response.getData());
-    // assertTrue(String.valueOf(response.getData()).contains("QUICKSTART_HOMEPAGE"));
+    assertTrue(String.valueOf(response.getData()).contains("QUICKSTART_HOMEPAGE"));
 	}
 
 }
